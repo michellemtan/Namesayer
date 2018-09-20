855504c045b9bf78fe4dcd075d908d941059398a
Michelle Tan <michellemtan@outlook.com>
2018-09-20 22:41:10 +1200

    Implement new loader in instructions menu

--- src/model/resources/InstructionsMenuController.java
+++ src/model/resources/InstructionsMenuController.java
@@ -20,9 +20,13 @@ public class InstructionsMenuController {
     //Return the user to the start menu when the back button is clicked
     @FXML
     void backButtonClicked(MouseEvent event) throws IOException {
-        Scene scene = SetUp.getInstance().startMenu;
-        Stage window = (Stage) backButton.getScene().getWindow();
-        window.setScene(scene);
+//        Scene scene = SetUp.getInstance().startMenu;
+//        Stage window = (Stage) backButton.getScene().getWindow();
+//        window.setScene(scene);
+
+//        // Load the new scene
+        Scene scene = backButton.getScene();
+        scene.setRoot(Menu.STARTMENU.loader().load());
 
     }
 

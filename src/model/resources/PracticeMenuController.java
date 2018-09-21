d9065b0f4566013a687950b091a02cce40a21154
Michelle Tan <michellemtan@outlook.com>
2018-09-21 22:47:03 +1200

    Finish play functionality!

--- src/model/resources/PracticeMenuController.java
+++ src/model/resources/PracticeMenuController.java
@@ -4,6 +4,8 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -35,6 +37,7 @@ public class PracticeMenuController {
     private List<String> creationList;
     private String selectedName;
     private String pathToDB;
+    private Iterator<String> playerIterator;
 
     //TODO: Dear Michelle,
     //TODO: Some fixes that must now be made to this class are:
@@ -163,25 +166,38 @@ public class PracticeMenuController {
 
     private void mediaPlayerCreator() throws IOException {
 
+        ObservableList<Media> mediaList = FXCollections.observableArrayList();
         String databasePath = SetUp.getInstance().dbMenuController.getPathToDB();
-        Media media = new Media(new File(databasePath+"/"+selectedName+"/"+selectedName).toURI().toString() + ".wav");
-        audioPlayer = new MediaPlayer(media);
-        audioPlayer.setOnPlaying(new AudioRunnable(false));
-        audioPlayer.setOnEndOfMedia(new AudioRunnable(true));
+
+        for (String creation : creationList) {
+            //Set up the file to be played
+            selectedName = creation;
+            Media media = new Media(new File(databasePath + "/" + selectedName + "/" + selectedName).toURI().toString() + ".wav");
+            mediaList.add(media);
+         }
+         playMediaTracks(mediaList);
+    }
+
+
+    private void playMediaTracks(ObservableList<Media> mediaList) {
+        if (mediaList.size() == 0) {
+            return;
+        }
+        audioPlayer = new MediaPlayer(mediaList.remove(0));
         audioPlayer.play();
 
-        audioPlayer.currentTimeProperty().addListener((ChangeListener) (observable, oldValue, newValue) -> {
-            Duration newDuration = (Duration) newValue;
-            progressBar.setProgress(newDuration.toSeconds()/5);
-
-        });
+        audioPlayer.setOnPlaying(new AudioRunnable(false));
+        audioPlayer.setOnEndOfMedia(new AudioRunnable(true));
         audioPlayer.setOnReady(() -> progressBar.setProgress(0.0));
-        /*audioPlayer.currentTimeProperty().addListener((ChangeListener) (observable, oldValue, newValue) -> {
-            Duration newDuration = (Duration) newValue;
-            progressBar.setProgress(newDuration.toSeconds()/5);
-
-        });*/
         audioPlayer.setOnReady(this::progressBar);
+
+        audioPlayer.setOnEndOfMedia(new Runnable() {
+            @Override
+            public void run() {
+                playMediaTracks(mediaList);
+            }
+        });
+
     }
 
     private void progressBar() {
@@ -193,9 +209,6 @@ public class PracticeMenuController {
         timeline.play();
     }
 
-
-
-    //TODO: Make this a public class?
     //AudioRunnable is a thread that runs in the background and acts as a listener for the media player to ensure buttons are enabled/disabled correctly
     private class AudioRunnable implements Runnable {
 
@@ -209,6 +222,7 @@ public class PracticeMenuController {
         public void run() {
             //When the media player has finished, the buttons will be enabled
             if (isFinished) {
+                selectedName = playerIterator.next();
                 backButton.setDisable(false);
                 recordButton.setDisable(false);
                 //When the media player is playing the audio file, the buttons will be disabled to prevent the user from navigating away

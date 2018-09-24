d636dd31421dee9255f825d22fe824aa158a41f6
Michelle Tan <michellemtan@outlook.com>
2018-09-24 23:40:42 +1200

    Added comments and fixed practice menu bug

--- myscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~
-
-ffmpeg -f alsa -i hw:0 -acodec libmp3lame -filter_complex showvolume=b=5:v=0:h=75:w=600[a] -map [a] -window_title "Microphone Volume" -f sdl - 2>/dev/null
-
---
+++ src/model/MicrophoneScriptGenerator.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.io.PrintWriter;
+
+public class MicrophoneScriptGenerator {
+
+    public void makeBashScript() {
+        PrintWriter writer = new PrintWriter("myscript.sh");
+        writer.println("#!/bin/bash");
+        writer.println("cd ~");
+        writer.println("ffmpeg -f alsa -i hw:0 -acodec libmp3lame -filter_complex showvolume=b=5:v=0:h=75:w=600[a] -map [a] -window_title \"Microphone Volume\" -f sdl - 2>/dev/null\n");
+        writer.close();
+    }
+}
